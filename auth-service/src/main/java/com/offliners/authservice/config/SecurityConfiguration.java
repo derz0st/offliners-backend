9922cf9674f303704d6ae68a7bf88bb841ff8530
Denys Kochubei <den.kochubey@gmail.com>
2018-08-27 21:38:53 +0300

    Setup public and non-public API configuration

---
+++ auth-service/src/main/java/com/offliners/authservice/config/SecurityConfiguration.java
@@ -0,0 +1,20 @@
+package com.offliners.authservice.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+
+                .antMatchers("/api/**").authenticated()
+                .antMatchers("/public/**").permitAll()
+                .anyRequest().authenticated()
+                .and().logout().logoutSuccessUrl("/").permitAll();
+    }
+}
